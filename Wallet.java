1e957150d2aabad2e34a31cf5e029fb51ba3b200
stingleyarana <stingleyarana@cdzhongguan.com>
2026-04-11 12:59:27 +0800

    Create Wallet.java

---
+++ Wallet.java
@@ -0,0 +1,29 @@
+package org.blockchain.wallet;
+
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
+
+public class Wallet {
+    public PrivateKey privateKey;
+    public PublicKey publicKey;
+
+    public Wallet() {
+        generateKeyPair();
+    }
+
+    private void generateKeyPair() {
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA", "BC");
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime256v1");
+            
+            keyGen.initialize(ecSpec, random);
+            KeyPair keyPair = keyGen.generateKeyPair();
+            
+            privateKey = keyPair.getPrivate();
+            publicKey = keyPair.getPublic();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
